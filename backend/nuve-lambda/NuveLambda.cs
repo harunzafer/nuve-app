76ece6662efb42e7ac746e7c322e588403ec818a
Harun Zafer <harunzafer.dev@gmail.com>
2023-02-25 15:16:57 -0500

    Implement .Net aws lambda with CDK

---
+++ backend/nuve-lambda/NuveLambda.cs
@@ -0,0 +1,26 @@
+﻿using Amazon.Lambda.Core;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace nuve_lambda
+{
+    public class Function
+    {
+
+        /// <summary>
+        /// A simple function that takes a string and returns both the upper and lower case version of the string.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public Casing FunctionHandler(string input, ILambdaContext context)
+        {
+            System.Console.WriteLine("Hello World");
+            return new Casing(input.ToLower(), input.ToUpper());
+            
+        }
+    }
+
+    public record Casing(string Lower, string Upper);
+}
